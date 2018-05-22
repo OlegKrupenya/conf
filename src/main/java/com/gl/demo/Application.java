122d253319b2fc762351bdc98d919fc199e3844e
Oleh Krupenia <oleh.krupenia@globallogic.com>
2018-05-22 22:33:15 +0300

    Add Java 10 support

--- src/main/java/com/gl/demo/Application.java
+++ src/main/java/com/gl/demo/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
         (scanBasePackages = {
                 "com.gl.demo.repository"})
@@ -21,6 +23,9 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) {
         repository.findAll().forEach(System.out::println);
+        var data = new ArrayList<String>();
+        data.add("123");
+        System.out.println(data.get(0));
     }
 
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author Oleg Krupenya
+ */
+module conf.main {
+    requires spring.data.mongodb;
+    requires spring.data.commons;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.beans;
+}
