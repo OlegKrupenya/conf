f8a9c3ed8a0cc8643275d3831fc255a2da8648b1
Oleh Krupenia <oleh.krupenia@globallogic.com>
2018-06-04 00:06:51 +0300

    split packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,5 @@
 /**
  * @author Oleg Krupenya
  */
-module conf.main {
-    requires spring.data.mongodb;
-    requires spring.data.commons;
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
-    requires spring.beans;
-    requires java.xml.bind;
+module conf {
 }
